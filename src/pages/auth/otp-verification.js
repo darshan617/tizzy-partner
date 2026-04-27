3b7d4dc9463a8eb4faf396e96dbaef4795168d82
darshan617 <darshan@goyalinfotech.com>
2026-04-27 17:29:49 +0530

    done with register and otp verfiy api integration

---
+++ src/pages/auth/otp-verification.js
@@ -0,0 +1,12 @@
+import VerifyOtp from "@/components/auth/verify-otp/VerifyOtp";
+import React from "react";
+
+const OTPVerification = () => {
+  return (
+    <div>
+      <VerifyOtp />
+    </div>
+  );
+};
+
+export default OTPVerification;
--- src/proxy.js
+++ src/proxy.js
@@ -11,9 +11,9 @@ export function proxy(request) {
   const isLoginAuthRoute = pathname === "/auth/login";
   const isSignupAuthRoute = pathname === "/auth/signup";
 
-  // if (!token && isHomeRoute) {
-  //   return NextResponse.redirect(new URL("/auth/login", request.url));
-  // }
+  if (!token && isHomeRoute) {
+    return NextResponse.redirect(new URL("/auth/login", request.url));
+  }
 
   if (token && isLoginAuthRoute) {
     return NextResponse.redirect(new URL("/", request.url));
