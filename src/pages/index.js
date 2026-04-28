5309efc2c18b6ea1df589b78e93e27dee6735f1d
Saif Shaikh <shaikhsaif01432@gmail.com>
2026-04-28 15:19:50 +0530

    worked on customer summary counter

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import Summary from "@/components/customers/summary/Summary";
 import AccountSummary from "@/components/dashboard/account-summary/AccountSummary";
 import Layout from "@/components/layout/Layout";
 import React from "react";
@@ -5,6 +6,7 @@ import React from "react";
 const Home = () => {
   return <Layout>
     <AccountSummary/>
+    <Summary />
   </Layout>;
 };
 
