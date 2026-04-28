6f3bf72370138e77d0d733f05ebdb54474a164a2
Saif Shaikh <shaikhsaif01432@gmail.com>
2026-04-28 18:30:16 +0530

    $style is pending

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
-import Summary from "@/components/customers/summary/Summary";
+// import Summary from "@/components/customers/summary/Summary";
 import AccountSummary from "@/components/dashboard/account-summary/AccountSummary";
+import TransactionSection from "@/components/dashboard/transaction/Transaction";
 import Layout from "@/components/layout/Layout";
 import React from "react";
 
 const Home = () => {
   return <Layout>
     <AccountSummary/>
-    <Summary />
+    {/* <Summary /> */}
+    <TransactionSection />
   </Layout>;
 };
 
