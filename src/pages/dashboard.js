60e04e4a5a2df2906c02cc05a11c19db5b3e46f0
Saif Shaikh <shaikhsaif01432@gmail.com>
2026-05-02 18:57:41 +0530

    aadded order summary design

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,4 +1,6 @@
 import CustomerDetail from "@/components/customers/customers-details/CustomersDetails";
+import OrderSummary from "@/components/customers/renew-plans/order-summary/OrderSummary";
+import RenewCart from "@/components/customers/renew-plans/renew-cart/RenewCart";
 import AccountSummary from "@/components/dashboard/account-summary/AccountSummary";
 import SalesReport from "@/components/dashboard/sales-report/SalesReport";
 import Support from "@/components/dashboard/support/Support";
