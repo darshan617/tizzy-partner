27800287428c6f40b627b5ee3fdc0d0f12ba98bc
darshan617 <darshan@goyalinfotech.com>
2026-05-11 15:14:09 +0530

    added update cart api

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,6 +4,7 @@ import { createWrapper } from "next-redux-wrapper";
 import { apiSlice } from "./apiSlice";
 import servicesSlice from "./slices/servicesSlice";
 import customerSlice from "./slices/customerSlice";
+import cartSlice from "./slices/cartSlice";
 
 const makeStore = () => {
   const store = configureStore({
@@ -11,6 +12,7 @@ const makeStore = () => {
       user: userSlice,
       services: servicesSlice,
       customer: customerSlice,
+      cart: cartSlice,
       [apiSlice.reducerPath]: apiSlice.reducer,
     },
     middleware: (getDefaultMiddleware) =>
