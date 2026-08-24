4256adcfb846af5907a40c30204790a1df6849bd
darshan617 <darshan@goyalinfotech.com>
2026-08-24 15:56:41 +0530

    done with adding same flow allover the website

--- src/components/subscription/subscriptions-details/SubscriptionsDetailsComponent.jsx
+++ src/components/subscription/subscriptions-details/SubscriptionsDetailsComponent.jsx
@@ -28,6 +28,7 @@ import {
 import { useDispatch, useSelector } from "react-redux";
 import CustomPopup from "@/common-components/custom-popup/CustomPopup";
 import { IoClose } from "react-icons/io5";
+import { GoPlus } from "react-icons/go";
 
 const planProviderIcons = [
   <svg
@@ -719,7 +720,7 @@ const SubscriptionsDetailsComponent = () => {
                             </div>
                           </div>
                           <button
-                            className={`btn small btnDefault`}
+                            className={styles.addLicenseBtn}
                             onClick={() => {
                               router?.push({
                                 pathname: "/order-summary",
@@ -733,7 +734,7 @@ const SubscriptionsDetailsComponent = () => {
                               });
                             }}
                           >
-                            Add
+                            <GoPlus size={14} /> Add
                           </button>
                         </div>
 
